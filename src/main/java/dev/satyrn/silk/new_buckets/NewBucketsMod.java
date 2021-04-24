70c1a3196b1c76f2689ba2fabb6939bea87a012b
Isabel Maskrey <isabel@satyrn.dev>
2021-04-24 16:23:21 -0400

    🚧 Add stub mod initializer

---
+++ src/main/java/dev/satyrn/silk/new_buckets/NewBucketsMod.java
@@ -0,0 +1,19 @@
+package dev.satyrn.silk.new_buckets;
+
+import net.fabricmc.api.ModInitializer;
+
+/**
+ * The initializer for the New Buckets mod.
+ * @author Isabel Maskrey
+ * @since 1.0.0
+ */
+public class NewBucketsMod implements ModInitializer {
+    /**
+     * Initializes the New Buckets mod.
+     * @since 1.0.0
+     */
+    @Override
+    public void onInitialize() {
+
+    }
+}
