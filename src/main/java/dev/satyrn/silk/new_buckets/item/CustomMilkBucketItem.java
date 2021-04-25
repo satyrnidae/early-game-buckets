9a5404da7a38fc5d57d7cef3be2e2001ea39061f
Isabel Maskrey <isabel@satyrn.dev>
2021-04-25 05:09:45 -0400

    ✨Update custom milk bucket item Copy enchants on state change, disable repair crafting, etc.

--- src/main/java/dev/satyrn/silk/new_buckets/item/CustomMilkBucketItem.java
+++ src/main/java/dev/satyrn/silk/new_buckets/item/CustomMilkBucketItem.java
@@ -1,11 +1,11 @@
 package dev.satyrn.silk.new_buckets.item;
 
+import dev.satyrn.silk.new_buckets.NewBucketsMod;
 import net.minecraft.advancement.criterion.Criteria;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
 import net.minecraft.item.MilkBucketItem;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.stat.Stats;
@@ -17,7 +17,7 @@ import net.minecraft.world.World;
  * @author Isabel Maskrey
  * @since 1.0.0
  */
-public abstract class CustomMilkBucketItem extends MilkBucketItem {
+public abstract class CustomMilkBucketItem extends MilkBucketItem implements DisableRepairItem {
 
     /**
      * Initializes the custom milk bucket class.
@@ -56,11 +56,10 @@ public abstract class CustomMilkBucketItem extends MilkBucketItem {
 
         ItemStack emptyBucket = stack.isEmpty() ? new ItemStack(this.getEmptyItem()) : stack;
         emptyBucket.setDamage(currentDamage);
+        NewBucketsMod.copyEnchantmentItemTags(stack, emptyBucket);
 
         if (user instanceof PlayerEntity) {
-            emptyBucket.damage(1, (PlayerEntity)user, (playerEntity) -> {
-                playerEntity.sendToolBreakStatus(hand);
-            });
+            emptyBucket.damage(1, (PlayerEntity)user, (playerEntity) -> playerEntity.sendToolBreakStatus(hand));
         }
 
         return emptyBucket.isEmpty() ? ItemStack.EMPTY : emptyBucket;
@@ -73,4 +72,26 @@ public abstract class CustomMilkBucketItem extends MilkBucketItem {
      */
     protected abstract Item getEmptyItem();
 
+    /**
+     * Checks if the item can be repaired with a given ingredient.
+     * @param stack The stack to repair.
+     * @param ingredient The ingredient to repair with.
+     * @return {@code false}, as milk buckets cannot be repaired.
+     * @since 1.0.0
+     */
+    @Override
+    public boolean canRepair(ItemStack stack, ItemStack ingredient) {
+        return false;
+    }
+
+    /**
+     * Disables repair of this item via the crafting grid.
+     * @param stack The item stack.
+     * @return {@code false}, as milk buckets cannot be repaired.
+     * @since 1.0.0
+     */
+    @Override
+    public boolean canRepairViaCrafting(ItemStack stack) {
+        return false;
+    }
 }
