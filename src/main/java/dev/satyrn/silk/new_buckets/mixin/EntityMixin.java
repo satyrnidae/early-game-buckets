1f3614140ebd40c55795bac8e23aa85928ad43dc
Isabel Maskrey <isabel@satyrn.dev>
2021-04-25 05:12:31 -0400

    ✨ Update entity mixin Add shadowed methods for descendant mixins

--- src/main/java/dev/satyrn/silk/new_buckets/mixin/EntityMixin.java
+++ src/main/java/dev/satyrn/silk/new_buckets/mixin/EntityMixin.java
@@ -1,8 +1,10 @@
 package dev.satyrn.silk.new_buckets.mixin;
 
 import net.minecraft.entity.Entity;
+import net.minecraft.sound.SoundEvent;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
 /**
@@ -19,4 +21,21 @@ public abstract class EntityMixin {
      */
     @Accessor("world")
     public abstract World getWorld();
+
+    /**
+     * Plays a sound from the entity.
+     * @param sound The sound to play.
+     * @param volume The volume at which to play the sound.
+     * @param pitch The pitch of the sound.
+     * @since 1.0.0
+     */
+    @Shadow
+    public abstract void playSound(SoundEvent sound, float volume, float pitch);
+
+    /**
+     * Removes an entity from the world.
+     * @since 1.0.0
+     */
+    @Shadow
+    public abstract void remove();
 }
