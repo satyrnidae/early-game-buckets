bed993321a6f8ccacba11371914d1e3d99a6a85c
Isabel Maskrey <isabel@satyrn.dev>
2021-04-25 05:11:14 -0400

    💡 Add comments for animal entity mixin

--- src/main/java/dev/satyrn/silk/new_buckets/mixin/AnimalEntityMixin.java
+++ src/main/java/dev/satyrn/silk/new_buckets/mixin/AnimalEntityMixin.java
@@ -3,7 +3,10 @@ package dev.satyrn.silk.new_buckets.mixin;
 import net.minecraft.entity.passive.AnimalEntity;
 import org.spongepowered.asm.mixin.Mixin;
 
+/**
+ * Provides hierarchy for animal entity mixins.
+ * @author Isabel Maskrey
+ * @since 1.0.0
+ */
 @Mixin(AnimalEntity.class)
-public abstract class AnimalEntityMixin extends PassiveEntityMixin {
-
-}
+public abstract class AnimalEntityMixin extends PassiveEntityMixin { }
