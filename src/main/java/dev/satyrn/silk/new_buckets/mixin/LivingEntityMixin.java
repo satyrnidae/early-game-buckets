86a7a3f5d9367a3b2b8c92f56e8272dd7c24dd8c
Isabel Maskrey <isabel@satyrn.dev>
2021-04-25 05:13:37 -0400

    ✨ Update living entity mixin Shadow methods for descendant mixins

--- src/main/java/dev/satyrn/silk/new_buckets/mixin/LivingEntityMixin.java
+++ src/main/java/dev/satyrn/silk/new_buckets/mixin/LivingEntityMixin.java
@@ -2,6 +2,7 @@ package dev.satyrn.silk.new_buckets.mixin;
 
 import net.minecraft.entity.LivingEntity;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 
 /**
  * Shadows and accessors for living entities.
@@ -10,4 +11,17 @@ import org.spongepowered.asm.mixin.Mixin;
  */
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin extends EntityMixin {
+    /**
+     * Gets a boolean flag indicating whether or not this entity is a baby.
+     * @return {@code true} if the entity is a baby; otherwise, {@code false}
+     * @since 1.0.0
+     */
+    @Shadow public abstract boolean isBaby();
+
+    /**
+     * Gets a boolean flag indicating whether or not the entity is alive.
+     * @return {@code true} if the entity is alive; otherwise, {@code false}
+     * @since 1.0.0
+     */
+    @Shadow public abstract boolean isAlive();
 }
